b92d52e12f2b4ca0fcb7d67959406c24e3063284
Ray Cole <raycolesmail@sbcglobal.net>
2019-10-09 07:24:58 -0700

    Edited config

--- data/SiteConfig.js
+++ data/SiteConfig.js
@@ -9,7 +9,6 @@ const config = {
   pathPrefix: "", // Prefixes all links. For cases when deployed to example.github.io/gatsby-advanced-starter/.
   siteDescription: "A Gatsby Markdown Blog stater with Advanced design in mind.", // Website description used for RSS feeds/meta description tag.
   siteRss: "/rss.xml", // Path to the RSS file.
-  siteFBAppID: "1825356251115265", // FB Application ID for using app insights
   googleAnalyticsID: "UA-47311644-5", // GA tracking ID.
   dateFromFormat: "YYYY-MM-DD", // Date format used in the frontmatter.
   dateFormat: "DD/MM/YYYY", // Date format for display.
